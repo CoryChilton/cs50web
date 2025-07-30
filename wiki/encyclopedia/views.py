513f79023c9857519c95146e0b231eb096c3d68f
Cory Chilton <corychilton@Corys-MacBook-Air.local>
2025-07-29 20:30:12 -0700

    finish project 1

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,70 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django import forms
+import random
+import markdown2
 
 from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+    entries = util.list_entries()
+    if 'q' not in request.GET:
+        return render(request, "encyclopedia/index.html", {
+            "entries": entries
+        })
+    
+    q = request.GET['q']
+    if q.lower() in [e.lower() for e in entries]:
+        print('yes')
+        return HttpResponseRedirect(reverse("entry", kwargs={'title': q}))
+    
+    results = [e for e in entries if q.lower() in e.lower()]
+    return render(request, "encyclopedia/search_results.html", {
+        "results": results
     })
 
+
+def entry(request, title):
+    entry = util.get_entry(title)
+    if not entry:
+        return render(request, "encyclopedia/error.html", {
+            "error": "Entry Not Found"
+        })
+
+    return render(request, "encyclopedia/entry.html", {
+        "title": title.capitalize(),
+        "entry": markdown2.markdown(entry)
+    })
+
+def new_page(request):
+    print(request.method)
+    if request.method == 'POST':
+        title = request.POST['title']
+        entries = util.list_entries()
+        if title.lower() in list(map(lambda e: e.lower(), entries)):
+            return render(request, "encyclopedia/error.html", {
+                "error": "An entry already exists with that title"
+        })
+        content = request.POST['content']
+        util.save_entry(title, content)
+        return HttpResponseRedirect(reverse("entry", kwargs={'title': title}))
+
+    return render(request, "encyclopedia/new_page.html")
+
+def edit_page(request, title):
+    if request.method == 'POST':
+        util.save_entry(title, request.POST['content'])
+        return HttpResponseRedirect(reverse("entry", kwargs={'title': title}))
+
+    entry = util.get_entry(title)
+    return render(request, "encyclopedia/edit_page.html", {
+        "title": title,
+        "entry": entry
+    })
+
+def random_page(request):
+    entries = util.list_entries()
+    return HttpResponseRedirect(reverse("entry", kwargs={"title": random.choice(entries)}))
+    
---
+++ wiki/entries/Markdown test.md
@@ -0,0 +1,7 @@
+# what
+## the 
+### helliante
+*is*
+_this_
+-markdown-
+__haha__
