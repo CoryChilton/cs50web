5cdacdc982c38ed0ce4a6948bfb02bf4f3c5e1d8
Cory Chilton <corychilton@Corys-MacBook-Air.local>
2025-08-03 18:34:34 -0700

    Add listings model

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    bid_price_cents = models.PositiveIntegerField()
+    image_url = models.URLField()
+    category = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.title
+
